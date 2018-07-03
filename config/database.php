21cfe7574b283341d3e48b96b7c0479f134d8246
dieg022 <diego.puya91@gmail.com>
2018-07-03 14:00:32 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'iptv'),
+            'database' => env('DB_DATABASE', ''),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'smartin1499'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
